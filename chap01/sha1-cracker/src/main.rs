e424addd1f2fede512615b24ae5aa2b585da4bbe
runtimejpp <jpnamausa@gmail.com>
2022-10-11 23:05:00 -0400

    new file:   chap01/sha1-cracker/Cargo.lock 	new file:   chap01/sha1-cracker/Cargo.toml 	new file:   chap01/sha1-cracker/src/main.rs 	new file:   chap01/sha1-cracker/src/readme.md "edit folder"

---
+++ chap01/sha1-cracker/src/main.rs
@@ -0,0 +1,45 @@
+
+use sha1::Digest; // calling crates Digest
+use std::{ 
+    env, // std env and error for errors, fs to talk to files like nodejs // and io for buffer communication 
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+}; 
+
+const SHA1_HEX_STRING_LENGTH: usize = 40; // 
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return Ok(());
+}
+
+
+let hash_to_crack = args[2].trim();
+
+if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH { // error handling in rust is straight firward on *some stuff
+    return Err("sha1 hash is not valid".into());
+}
+
+let wordlist_file = File::open(&args[1])?; // this is the file i am regading in the comments above 
+let reader = BufReader::new(&wordlist_file);
+    
+for line in reader.lines() {
+    let line = line?;
+    let common_password = line.trim(); 
+    if hash_to_crack ==
+    ↪
+        &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+        println!("Password found: {}", &common_password);
+        return Ok(()); // the exit of a successfull Rust program is return Ok(())
+        }
+    }
+
+    println!("password not found in wordlist :(");  // unsuccessful return Ok(()) != Ok(())
+    Ok(()) // unsuccessful program is expressed as Ok(())
+
+}
---
+++ chap01/sha1-cracker/src/readme.md
@@ -0,0 +1,7 @@
+$ cargo run -- wordlist.txt 3545436243634436tewtgggr34tgrrty3 <----- arbitrary and just for example 
+
+
+
+
+disclaimer
+learning purposes only 
